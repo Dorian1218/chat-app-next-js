3d2d7d86e339241fe526430e72f6814a593e334b
Dorian1218 <dmitchell1663@gmail.com>
2023-11-15 16:43:53 -0500

    reversed next config back to what it was originally, fixed serversession page

--- app/libs/serversession/page.jsx
+++ app/libs/serversession/page.jsx
@@ -1,4 +1,4 @@
-"use server"
+"use client"
 
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {reactStrictMode: false}
+const nextConfig = {reactStrictMode: false}
 
-// module.exports = nextConfig
-
-module.exports = {experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  }, reactStrictMode: false}
+module.exports = nextConfig
